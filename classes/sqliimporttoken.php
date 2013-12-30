1a14d95b5aeeb762ea7c6588fa2076a3ec2a9c0c
Simone D'Amico <info@simonedamico.com>
2013-12-30 10:05:26 +0100

    images

--- classes/sqliimporttoken.php
+++ classes/sqliimporttoken.php
@@ -62,6 +62,8 @@ class SQLIImportToken extends eZPersistentObject
             'value' => self::IMPORT_STATUS_RUNNING
         );
         $statusCount = parent::count( self::definition(), $conds );
+
+        var_dump($statusCount);
         
         return $statusCount > 0;
     }
